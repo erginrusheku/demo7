71b3d47bcd299257d886949ea95cd1d2411f10fe
Ergin Rusheku <rushekuergin@gmail.com>
2024-02-20 14:14:42 +0100

    modelMapper

--- src/main/java/group/demo7/Demo7Application.java
+++ src/main/java/group/demo7/Demo7Application.java
@@ -1,7 +1,9 @@
 package group.demo7;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Demo7Application {
@@ -10,4 +12,8 @@ public class Demo7Application {
 		SpringApplication.run(Demo7Application.class, args);
 	}
 
+	@Bean
+	public ModelMapper modelMapper(){
+		return new ModelMapper();
+	}
 }
