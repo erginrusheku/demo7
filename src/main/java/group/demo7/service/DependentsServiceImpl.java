44826a34b0d9ac66de8e62b9fe06ec32a8a37455
Ergin Rusheku <rushekuergin@gmail.com>
2024-02-20 12:18:03 +0100

    changes

--- src/main/java/group/demo7/service/DependentsServiceImpl.java
+++ src/main/java/group/demo7/service/DependentsServiceImpl.java
@@ -1,10 +1,16 @@
 package group.demo7.service;
 
 import group.demo7.dto.DependentsDTO;
+import group.demo7.repository.DependentsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
 public class DependentsServiceImpl implements DependentsService{
+
+    @Autowired
+    DependentsRepository dependentsRepository;
+
     @Override
     public List<DependentsDTO> dependentsList() {
         return null;
